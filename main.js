9ffa399ca2083535791e40d521dce52034d1dbea
yjj639 <88853937+yjj639@users.noreply.github.com>
2023-07-14 19:56:35 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
 const listDiv = document.querySelector(".list");
 const username = document.getElementById("username");
-
-var requestURL = "data.json";
-var data;
+var parsedData;
+var requestURL = "https://yjj639.github.io/data.json";
+var textURL = "textree\notes\EXAMPLE NOTE1.txt";
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 
 request.onload = function() {
-    data = request.response;
-    console.log(data);
+    var data = request.response;
+    console.log("request.onload 안");
+    console.log(data["user"]);
+    username.innerText = data["user"];
+    parsedData = JSON.parse(JSON.stringify(data));
+    
+    parsedData["titles"].forEach(element => {
+        var item = document.createElement("div");
+        item.className = "item";
+        item.innerHTML = `<a href="note.html">${element}</a>`;
+        listDiv.append(item);
+    });
 }
 
-username.innerHTML = data["user"];
-data["titles"].forEach(element => {
-    const item = document.createElement("div");
-    item.className = "item";
-    item.innerHTML = element;
-    listDiv.append(item);
-});
+var textReq = new XMLHttpRequest();
+textReq.open("GET", textURL);
+textReq.responseType = 'text';
+textReq.send();
+
+textReq.onload = function() {
+    console.log("textReq 안");
+    console.log(textReq.response);
+}
