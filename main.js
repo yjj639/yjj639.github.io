e662e7cdd67ec557b92458fd0cf8ad72700a6aee
yjj639 <88853937+yjj639@users.noreply.github.com>
2023-07-14 18:07:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
+const listDiv = document.querySelector(".list");
+const username = document.getElementById("username");
+
 var requestURL = "data.json";
+var data;
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 
 request.onload = function() {
-    var superHeroes = request.response;
-    console.log(superHeroes);
-  }
+    data = request.response;
+    console.log(data);
+}
 
-  
+username.innerHTML = data["user"];
+data["titles"].forEach(element => {
+    const item = document.createElement("div");
+    item.className = "item";
+    item.innerHTML = element;
+    listDiv.append(item);
+});
